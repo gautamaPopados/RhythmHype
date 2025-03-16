7fb1c12ade16e7754cc8d0c8d4e554a493d64398
gautamaPopados <155830875+gautamaPopados@users.noreply.github.com>
2025-03-16 21:54:47 +0700

    Update PlayerMovement.cs

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -20,29 +20,21 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        //управление
         float horizontalInput = horizontalAxis * MoveSpeed;
         float VerticalInput = verticalAxis * MoveSpeed;
 
-        //направление камеры
         Vector3 cameraForward = cam.forward;
         Vector3 cameraRight = cam.right;
 
         cameraForward.y = 0;
         cameraRight.y = 0;
 
-        //создаю относительное направление камеры
-
         Vector3 forwardRelative = VerticalInput * cameraForward;
         Vector3 rightRelative = horizontalInput * cameraRight;
         Vector3 moveDirection = forwardRelative + rightRelative;
 
-        //движение
-
         if (isRunning)
             rb.velocity = new Vector3(moveDirection.x, rb.velocity.y, moveDirection.z);
-
-        //if (Input.GetButtonDown("Jump") && Mathf.Approximately(rb.velocity.y, 0)) rb.velocity = new Vector3(rb.velocity.x, Jump, rb.velocity.z);
 
         if (isRunning)
             transform.forward = new Vector3(rb.velocity.x, 0, rb.velocity.z);
