9acf90e062628cea770ea5d9edfc9b01725f44c2
gautamaPopados <155830875+gautamaPopados@users.noreply.github.com>
2025-03-16 21:55:18 +0700

    Update PlayerMovementINTRO.cs

--- Assets/Player/PlayerMovementINTRO.cs
+++ Assets/Player/PlayerMovementINTRO.cs
@@ -16,24 +16,18 @@ public class PlayerMovementINTRO : MonoBehaviour
 
     void Update()
     {
-        //управление
         float horizontalInput = Input.GetAxisRaw("Horizontal") * MoveSpeed;
         float VerticalInput = Input.GetAxisRaw("Vertical") * MoveSpeed * 0;
 
-        //направление камеры
         Vector3 cameraForward = cam.forward;
         Vector3 cameraRight = cam.right;
 
         cameraForward.y = 0;
         cameraRight.y = 0;
 
-        //создаю относительное направление камеры
-
         Vector3 forwardRelative = VerticalInput * cameraForward;
         Vector3 rightRelative = horizontalInput * cameraRight;
         Vector3 moveDirection = forwardRelative + rightRelative;
-
-        //движение
 
         rb.velocity = new Vector3(moveDirection.x, rb.velocity.y, moveDirection.z);
 
